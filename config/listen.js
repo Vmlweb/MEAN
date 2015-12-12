66d93eff6a41ce29ad577d4a4bafbdf67896fe90
Victor Leach <victor96@mac.com>
2015-12-12 16:36:59 +0000

    No message

--- config/listen.js
+++ config/listen.js
@@ -10,8 +10,8 @@ module.exports = {
 		hostname: '0.0.0.0',
 		port: '4434',
 		ssl: {
-			key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-			cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
+			key: '',//fs.readFileSync(path.join(__dirname, 'key.pem')),
+			cert: ''//fs.readFileSync(path.join(__dirname, 'cert.pem'))
 		}
 	},
 	tests: {
