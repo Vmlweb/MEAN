0baef53e11e19e04cae467f8025ad3d963b4117a
Victor Leach <victor96@mac.com>
2015-12-16 16:27:46 +0000

    Tests can now access the Mongo database.

--- config/mongo.js
+++ config/mongo.js
@@ -4,7 +4,7 @@ module.exports = {
 	connection: {
 		user: 'mean',
 		password: 'mean',
-		hostname: process.env.MONGO_PORT_27017_TCP_ADDR,
+		hostname: process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost',
 		port: 27017,
 		database: 'mean'
 	}
